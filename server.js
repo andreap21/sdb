a4840ea2c5c9a9585e84eab44469bb07cad219ef
Andrea Piscopo <andreapiscopo21@gmail.com>
2025-12-03 22:40:06 +0100

    hytp

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
 
+const fs = require('fs');
+const https = require('https');
+
 const app = express();
 
 // --- STATIC FILES ---
 app.use(express.static(path.join(__dirname)));
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+// SSL Options
+const sslOptions = {
+    key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
+};
+
+// Create HTTPS Server
+https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`✅ HTTPS Server running on port ${PORT}`);
+});
